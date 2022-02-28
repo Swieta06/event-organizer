0a83be47dddebf933cf2da1170e77b44e2e96422
haiqal47 <haiqalramanizaralfajri@gmail.com>
2022-02-28 15:16:38 +0700

    fix error Theme model

--- models/Theme.js
+++ models/Theme.js
@@ -8,10 +8,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Theme.hasMany(models.Order);
     }
   }
-  Themes.init(
+  Theme.init(
     {
       name: DataTypes.STRING,
       photo: DataTypes.TEXT,
