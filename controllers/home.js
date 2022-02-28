efed8e6261c05fbc2e16e391c64c0eb935b9b615
Muhammad Yusuf <yusufmarsudien10@gmail.com>
2022-02-28 18:06:47 +0700

    Modified homeController

--- controllers/home.js
+++ controllers/home.js
@@ -2,8 +2,10 @@ const { Package } = require("../models");
 
 exports.getIndex = async (req, res) => {
   try {
-    const getAllPackage = await Package.findAll();
-    res.render("pages/index", { data: getAllPackage });
+    const getPackage = await Package.findAll({
+      attributes: ["id", "name", "price", "features"],
+    });
+    res.render("pages/index", { data: getPackage });
   } catch (error) {
     console.log(error);
   }
