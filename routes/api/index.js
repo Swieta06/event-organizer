2d6bca03f1cccdf62825320e8ae7db748b724119
kusno <intern.kusno@privyid.tech>
2022-03-01 15:38:05 +0700

    delete maps

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,12 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const userRoute = require('./user');
+
+router.use('/users', userRoute);
 
 /* GET test api. */
-router.get("/", function (req, res, next) {
-  res.status(200).json({ test: "test" });
+router.get('/', function (req, res, next) {
+  res.status(200).json({ test: 'test' });
 });
 
 module.exports = router;
