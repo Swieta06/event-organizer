cf220882a6511e1eaf2b544d25e5258ea06a0b18
haiqal47 <haiqalramanizaralfajri@gmail.com>
2022-03-04 02:09:18 +0700

    fix index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -13,22 +13,6 @@ router.use("/vendors", vendorRoute);
 
 /* GET test api. */
 router.get("/test", async function (req, res, next) {
-  const order = await Order.create({
-    UserId: "f8f8f8f8-f8f8-f8f8-f8f8-f8f8f8f8f8f8",
-    PackageId: "a7a7a7a7-a7a7-a7a7-a7a7-a7a7a7a7a7a7",
-    PaymentMethodId: 1,
-    customerName: "John Doe",
-    companyName: "John Doe",
-    tel: "0912345678",
-    address: "123 Fake Street",
-    postalCode: "12345",
-    status: 0,
-    totalPrice: 0,
-    totalParticipant: 0,
-    desc: "",
-    concept: "indoor",
-    eventAt: new Date(),
-  });
   res.status(200).json({ test: "test" });
 });
 
