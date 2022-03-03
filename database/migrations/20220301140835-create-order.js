fa37afb4f6c31b7e8d154dc9ac5975d10b791bef
haiqal47 <haiqalramanizaralfajri@gmail.com>
2022-03-04 02:07:43 +0700

    add hooks to generate custom id order

--- database/migrations/20220301140835-create-order.js
+++ database/migrations/20220301140835-create-order.js
@@ -5,7 +5,7 @@ module.exports = {
       id: {
         allowNull: false,
         primaryKey: true,
-        type: Sequelize.UUID,
+        type: Sequelize.STRING,
       },
       UserId: {
         type: Sequelize.UUID,
