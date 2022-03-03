252afa78109fa66b3eff554747d23508e520aed3
M-Fajar <m.fajar980527@gmail.com>
2022-03-03 22:21:02 +0700

    Post Order Payment

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.use(logger("dev"));
 
 // body parser setup
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // static files setup
