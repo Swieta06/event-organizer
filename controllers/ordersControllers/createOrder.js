1b2cfda8573f30ed44c61cb912dd37db448dd8d2
haiqal47 <haiqalramanizaralfajri@gmail.com>
2022-03-04 18:10:21 +0700

    add createOrder.js controller

---
+++ controllers/ordersControllers/createOrder.js
@@ -0,0 +1,64 @@
+const { Order, Package, OrderProduct } = require("../../models");
+const { Op } = require("sequelize");
+const response = require("../../utils/response");
+const createError = require("http-errors");
+
+exports.createOrder = async (req, res, next) => {
+  try {
+    const {
+      package,
+      totalParticipant,
+      totalPrice,
+      eventAt,
+      concept,
+      desc,
+      products,
+    } = req.body;
+
+    const { id: userId } = req.user;
+
+    const editedProducts = products.map((product) => {
+      return {
+        ProductId: product.id,
+        qty: product.qty,
+      };
+    });
+
+    const selectedPackage = await Package.findOne({
+      attributes: ["id"],
+      where: { [Op.or]: [{ name: package }, { slug: package }] },
+    });
+    const order = await Order.create(
+      {
+        UserId: userId,
+        totalParticipant: totalParticipant,
+        totalPrice: totalPrice,
+        eventAt: eventAt,
+        concept: concept,
+        desc: desc,
+        concept: concept,
+        PackageId: selectedPackage.id,
+        OrderProducts: editedProducts,
+      },
+      {
+        include: [OrderProduct],
+      }
+    );
+    const result = {
+      id: order.id,
+      totalParticipant: order.totalParticipant,
+      totalPrice: order.totalPrice,
+      eventAt: order.eventAt,
+      concept: order.concept,
+      desc: order.desc,
+      status: order.status,
+      package: selectedPackage.name,
+      updatedAt: order.updatedAt,
+      createdAt: order.createdAt,
+      redirectUrl: `/orders/${order.id}?step=3`,
+    };
+    res.status(200).json(response("success", result));
+  } catch (error) {
+    next(error);
+  }
+};
