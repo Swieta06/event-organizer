caba330d661cdd9b5aa024a74ffb88ffdbee4ead
swietanurjanah <swietanurjanah@gmail.com>
2022-03-04 11:39:05 +0700

    push new nav script

--- routes/web/index.js
+++ routes/web/index.js
@@ -6,6 +6,11 @@ const userRoute = require("./userRoute");
 const ordersRoute = require("./orders");
 const mainRoute = require('./main');
 
+// get home page
+router.get("/", (req,res,next)=>{
+res.render("pages/index",{user:req.user})
+})
+
 /* Main page. */
 router.use("/", mainRoute);
 
