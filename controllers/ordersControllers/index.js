048d2fa886cd0e3234695f9e4255485076cccea9
hamidbae <hamid1bae1@gmail.com>
2022-03-05 08:39:57 +0700

    add get step 3

--- controllers/ordersControllers/index.js
+++ controllers/ordersControllers/index.js
@@ -1,10 +1,12 @@
 const { getViews } = require("./getViews");
 const { getViewsStep2 } = require("./getViewsStep2");
-const { updateOrder } = require('./updateOrder');
+const { getViewsStep3 } = require("./getViewsStep3");
+const { updateOrder } = require("./updateOrder");
 const orderPayment = require("./orderPayment");
 module.exports = {
   getViews,
   getViewsStep2,
+  getViewsStep3,
   orderPayment,
   updateOrder,
 };
--- routes/web/orders.js
+++ routes/web/orders.js
@@ -6,6 +6,9 @@ const upload = require("../../middlewares/uploadPayment");
 /* GET Orders Page Step 1. */
 router.get("/", ordersControlers.getViews, ordersControlers.getViewsStep2);
 
+/* GET Orders Page Step 3. */
+router.get("/:idOrder/", ordersControlers.getViewsStep3);
+
 /* Update order after click next step on page step 3 to step 4 */
 router.post("/:idOrder/edit", ordersControlers.updateOrder);
 
