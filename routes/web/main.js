1f871a3dd8c44f6d0bda9874c4939820837e3a44
yusufprasatya <yusuf.marsudien10@gmail.com>
2022-03-07 22:12:04 +0700

    add getEvent Controller

--- routes/web/main.js
+++ routes/web/main.js
@@ -5,8 +5,8 @@ router.get("/", homeControllers.getIndex);
 router.get("/faq", (req, res)=>{
     res.render("pages/faq");
 });
-router.get("/events", homeControllers.getIndex);
-router.get("/about", homeControllers.getIndex);
-router.get("/find", homeControllers.getIndex);
+router.get("/events", homeControllers.getEvents);
+router.get("/about", homeControllers.getAbout);
+router.get("/find", homeControllers.getFind);
 
 module.exports = router;
