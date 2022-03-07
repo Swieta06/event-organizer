9c53db9bcba02f7004ce4da7c89fa8c5baec3316
kartikanandap <kartikanandap@gmail.com>
2022-03-08 00:14:26 +0700

    getPackage. Done

--- controllers/packageControllers/getPackage.js
+++ controllers/packageControllers/getPackage.js
@@ -3,7 +3,18 @@ const { Package } = require("../../models");
 
 async function getPackage(req, res, next) {
     try {
-        
+        const {
+            id
+        } = req.params;
+
+        const options = {
+            where: {
+                id
+            }
+        }
+
+        const payload = await Package.findOne(options);
+        res.status(200).json(response("Success", payload));
     } catch (error) {
         next(error);
     }
