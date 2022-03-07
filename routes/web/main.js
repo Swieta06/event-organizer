ca5003c14e54cef2036bcfc83d093be5fe47915b
yusufprasatya <yusuf.marsudien10@gmail.com>
2022-03-07 22:33:42 +0700

    Modified route static

--- routes/web/main.js
+++ routes/web/main.js
@@ -2,9 +2,6 @@ const { homeControllers } = require("../../controllers");
 const router = require("express").Router();
 
 router.get("/", homeControllers.getIndex);
-router.get("/faq", (req, res)=>{
-    res.render("pages/faq");
-});
 router.get("/events", homeControllers.getEvents);
 router.get("/about", homeControllers.getAbout);
 router.get("/find", homeControllers.getFind);
