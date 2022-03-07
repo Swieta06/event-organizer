759926a20577f50d819528fc0c2419cdb7dd0011
kartikanandap <kartikanandap@gmail.com>
2022-03-08 00:26:46 +0700

    deletePackage. Done

--- controllers/packageControllers/deletePackage.js
+++ controllers/packageControllers/deletePackage.js
@@ -1,8 +1,21 @@
-
+const response = require('../../utils/response');
+const { Package } = require("../../models");
 
 async function deletePackage(req, res, next) {
     try {
-        
+        const {
+            id
+        } = req.params;
+
+        const options = {
+            where: {
+                id
+            },
+            returning: true
+        }
+
+        const payload = await Package.destroy(options);
+        res.status(200).json(response("Success", payload));
     } catch (error) {
         next(error);
     }
