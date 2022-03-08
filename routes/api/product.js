58b70699b5c996b84e351d72f3b8456a931ada25
M-Fajar <m.fajar980527@gmail.com>
2022-03-08 20:13:19 +0700

    Fix UpdateImageProduct

--- routes/api/product.js
+++ routes/api/product.js
@@ -5,7 +5,7 @@ const upload = require("../../middlewares/uploadImageProduct");
 router.post("/", upload.single("photo"), productController.createProduct);
 router.get("/", productController.getAllProduct);
 router.get("/:id", productController.getProductById);
-router.put("/:id", productController.updateProduct);
+router.put("/:id", upload.single("photo"), productController.updateProduct);
 router.delete("/:id", productController.deleteProduct);
 router.get("/vendors/:vendorId", productController.getByVendor);
 
