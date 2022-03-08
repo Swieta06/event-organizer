e6bbd5b8644f025c3ec9a1984275578055f9c42f
yusufprasatya <yusuf.marsudien10@gmail.com>
2022-03-08 16:46:34 +0700

    Add orderDetail controller

--- controllers/ordersControllers/orderDetail.js
+++ controllers/ordersControllers/orderDetail.js
@@ -5,16 +5,16 @@ const {
   Category,
   Payment,
   Vendor,
+  OrderProduct,
 } = require("../../models");
 const response = require("../../utils/response");
 const createError = require("http-errors");
 const { Op } = require("@sequelize/core");
-const { getByVendor } = require("../productControllers");
 
 exports.orderDetail = async (req, res, next) => {
   try {
     const { idOrder } = req.params;
-    let orderDetail = await Order.findOne({
+    const orderDetail = await Order.findOne({
       where: {
         id: idOrder,
       },
@@ -47,18 +47,24 @@ exports.orderDetail = async (req, res, next) => {
         },
         {
           model: Product,
-            as: "products",
-          attributes: ["id", "name"],
+          as: "products",
+          through: {
+            attributes: ["qty"],
+          },
+
+          attributes: ["id", "name", "CategoryId"],
           include: [
+            {
+              model: Category,
+              as: "category",
+              attributes: ["id", "name"],
+            },
             {
               model: Vendor,
               as: "vendor",
               attributes: ["name"],
             },
           ],
-          through: {
-            attributes: ["qty"],
-          },
         },
         {
           model: Payment,
@@ -73,24 +79,44 @@ exports.orderDetail = async (req, res, next) => {
         },
       ],
     });
+    // const orderDetails = orderDetail.dataValues.products.map((el) => {
+    //   const productDetail = {};
+    //   productDetail.category = el.category.name;
+    //   productDetail.id = el.id;
+    //   productDetail.name = el.name;
+    //   productDetail.vendor = el.vendor.name;
+    //   productDetail.qty = el.OrderProduct.qty;
 
-    console.log(orderDetail.dataValues.products[0].vendor.name, "<<<<<<");
+    //   if (!productDetail[el.category.name]) {
+    //     productDetail[el.category.name] = [];
+    //   }
+    //   productDetail[el.category.name].push(productDetail);
+    //   // console.log(el.products.CategoryId, "ini id apa?");
+    //   console.log(el.category.id, "ini id Category");
+    //   return productDetail;
+    // });
+    // delete orderDetail.dataValues.products;
+    // orderDetail.dataValues.Products = orderDetails;
 
-    const orderDetails = orderDetail.dataValues.products.map((el) => {
-      const productDetail = {};
-      productDetail.id = el.id;
-      productDetail.name = el.name;
-      productDetail.vendor = el.vendor.name;
-      productDetail.qty = el.OrderProduct.qty;
+    let products = {};
 
-      return productDetail;
+    orderDetail.products.forEach((el) => {
+      if (!products[el.category.name]) {
+        products[el.category.name] = [];
+      }
+      products[el.category.name].push({
+        id: el.id,
+        name: el.name,
+        vendor: el.vendor.name,
+        qty: el.OrderProduct.qty,
+      });
     });
-    // orderDetail.dataValues.products = orderDetails;
 
-    console.log(orderDetail, "<<<< Ini Order Detail");
-
+    orderDetail.dataValues.products = products;
+    orderDetail.products = products;
+    console.log(orderDetail, "Ini Datavalues");
     if (orderDetail) {
-      res.status(200).json(orderDetail);
+      res.render("pages/detailOrder", orderDetail);
     } else {
       res.status(404).json(response("Not Found"));
     }
