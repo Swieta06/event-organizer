028eb9d16e776931ee08afddd68fddde09f2f037
BryanRJR <bryanrantung98@gmail.com>
2022-03-09 19:06:20 +0800

    confirm payment test 2

--- public/javascripts/confirm-payment.js
+++ public/javascripts/confirm-payment.js
@@ -1,3 +1,4 @@
+// javascript copy number rek
 function copyNumber(value) {
   var tempInput = document.createElement('input');
   tempInput.value = value;
@@ -10,3 +11,16 @@ function copyNumber(value) {
 document.querySelector('#btn-bca').onclick = function () {
   copyNumber('382371928');
 };
+
+// javascript upload Image
+const upload = document.querySelector('#upload');
+var uploaded_image;
+
+upload.addEventListener('change', function () {
+  const reader = new FileReader();
+  reader.addEventListener('load', () => {
+    uploaded_image = reader.result;
+    document.querySelector('#inner-border').style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
