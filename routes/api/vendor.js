669d98801953d29a5de3fff85fc6aa4c516f6660
M-Fajar <m.fajar980527@gmail.com>
2022-03-09 21:57:02 +0700

    edit login google

--- routes/api/vendor.js
+++ routes/api/vendor.js
@@ -4,9 +4,9 @@ const vendorController = require("../../controllers/vendorControllers");
 const authentication = require("../../middlewares/authentication");
 const authenticationAdmin = require("../../middlewares/authenticationAdmin");
 
-router.use(authentication, authenticationAdmin);
-
+router.use(authentication);
 router.get("/", vendorController.getAllVendor);
+router.use(authenticationAdmin);
 router.post("/", vendorController.createVendor);
 router.get("/:id", vendorController.getVendorById);
 router.put("/:id", vendorController.updateVendor);
