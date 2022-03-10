66352a4a60cc7fa0223dcd5052714d55b3382136
BryanRJR <bryanrantung98@gmail.com>
2022-03-10 12:22:37 +0800

    update orderStep4

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const userRoute = require("./user");
-const productRoute = require("./product");
-const adminRoute = require("./admin");
-const vendorRoute = require("./vendor");
-const orderRoute = require("./order");
+const userRoute = require('./user');
+const productRoute = require('./product');
+const adminRoute = require('./admin');
+const vendorRoute = require('./vendor');
+const orderRoute = require('./order');
 const pakcageRoute = require('./package');
 
-router.use("/admin", adminRoute);
-router.use("/users", userRoute);
-router.use("/products", productRoute);
-router.use("/vendors", vendorRoute);
-router.use("/orders", orderRoute);
-router.use("/packages", pakcageRoute);
+router.use('/admin', adminRoute);
+router.use('/users', userRoute);
+router.use('/products', productRoute);
+router.use('/vendors', vendorRoute);
+router.use('/orders', orderRoute);
+router.use('/packages', pakcageRoute);
 
 /* GET test api. */
 router.get('/', function (req, res, next) {
