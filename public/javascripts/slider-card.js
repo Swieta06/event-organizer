795cf5fbb1a2c6a6167a855cb7e01f013fde2814
Ari Cahyono <cahyono.ari81@gmail.com>
2022-03-12 13:01:22 +0700

    Refactor : Nav OrderList & add link

--- public/javascripts/slider-card.js
+++ public/javascripts/slider-card.js
@@ -1,19 +1,26 @@
-function sliderControl(title) {
+const addTransform = (dataLength, hitung, transformValue, title) => {
+  for (let i = 1; i <= dataLength; i++) {
+    const elm = document.getElementById(`${title + i}`);
+    elm.style.transform = `translate(-${hitung * transformValue}%, 0%)`;
+    elm.style.transition = "all 1s ease 0s";
+  }
+};
+
+function sliderControl(title, totalItemShow = 5, transformValue = 104) {
   const pivot = document.querySelector(`.${"pivot" + title}`).id;
   const sortingId = parseInt(pivot.charAt(pivot.length - 1));
   const dataLength = document.querySelectorAll(
     `.slide-show-item-${title}`
   ).length;
-  const totalItemShow = 5;
-
   const btnLeft = document.getElementById(`btnLeft${title}`);
   const btnRight = document.getElementById(`btnRight${title}`);
 
   if (sortingId === 1) {
     btnLeft.style.visibility = "hidden";
   }
+  const pivotId = parseInt(pivot.charAt(pivot.length - 1));
 
-  if (dataLength <= 5) {
+  if (dataLength <= totalItemShow || pivotId > dataLength - totalItemShow) {
     btnRight.style.visibility = "hidden";
   }
 
@@ -23,13 +30,8 @@ function sliderControl(title) {
     const pivotBreak = dataLength - totalItemShow + 1;
     const hitung = getData > pivotBreak ? pivotBreak : getData;
 
-    for (let i = 1; i <= dataLength; i++) {
-      document.getElementById(`${title + i}`).style.transform = `translate(-${
-        hitung * 104
-      }%, 0%)`;
-      document.getElementById(`${title + i}`).style.transition =
-        "all 1s ease 0s";
-    }
+    addTransform(dataLength, hitung, transformValue, title);
+
     const addPivot = getData + 1;
     if (getData <= pivotBreak) {
       document.getElementById(pivot).classList.remove(`${"pivot" + title}`);
@@ -53,13 +55,8 @@ function sliderControl(title) {
     const hitung = getData - 2;
     const rubah = getData - 1;
 
-    for (let i = 1; i <= dataLength; i++) {
-      document.getElementById(`${title + i}`).style.transform = `translate(-${
-        hitung * 104
-      }%, 0%)`;
-      document.getElementById(`${title + i}`).style.transition =
-        "all 1s ease 0s";
-    }
+    addTransform(dataLength, hitung, transformValue, title);
+
     if (getData > 1) {
       document.getElementById(pivot).classList.remove(`${"pivot" + title}`);
       document
@@ -73,14 +70,14 @@ function sliderControl(title) {
       btnLeft.style.visibility = "hidden";
     }
   });
-};
+}
 
-   const viewBtnSlider = (title) => {
-     return `
+const viewBtnSlider = (title) => {
+  return `
             <div class="slide-show-control">
                 <button id="btnLeft${title}" class=" fs-18 btn text-start bg-neutral-10 shadow  rounded-circle"><i class="bi bi-chevron-left"></i></button>
                 <button id="btnRight${title}" class=" fs-18 btn text-end bg-neutral-10 shadow rounded-circle  top-0 left-0"><i class="bi bi-chevron-right"></i></button>
              </div>
              <div class="slide-show-ari theme-check"  id="${title}Item"></div>
                       `;
-   };
+};
