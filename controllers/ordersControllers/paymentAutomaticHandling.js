0eb2d06374ae0c08fa276d54b9c3d12f55de4f8c
M-Fajar <m.fajar980527@gmail.com>
2022-03-13 09:38:17 +0700

    Add FixOrder UpdateOrder

--- controllers/ordersControllers/paymentAutomaticHandling.js
+++ controllers/ordersControllers/paymentAutomaticHandling.js
@@ -2,12 +2,10 @@ const { User, Order, MidtransPayment } = require("../../models");
 const createError = require("http-errors");
 
 async function paymentHandling(req, res) {
-  const { OrderId, va_number, pdf_url, transaction_status } = req.body;
+  const { OrderId, pdf_url } = req.body;
 
   const payload = {
-    va_number,
     pdf_url,
-    transaction_status,
   };
 
   const midtransUpdated = await MidtransPayment.update(payload, {
@@ -15,20 +13,7 @@ async function paymentHandling(req, res) {
       OrderId,
     },
   });
-  console.log(midtransUpdated);
-  let statusOrder = 1;
-  if (transaction_status == "settlement" || transaction_status == "capture") {
-    statusOrder = 3;
-  }
 
-  const updateStatusOrder = await Order.update(
-    { status: statusOrder },
-    {
-      where: {
-        id: OrderId,
-      },
-    }
-  );
   res.sendStatus(200);
 }
 
